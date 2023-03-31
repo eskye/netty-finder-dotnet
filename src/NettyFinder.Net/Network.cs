f02b2d5f3cd8887ba1c66676b9f28f52442f62ad
Sunkanmi Ijatuyi <sunkanmiijatuyi@gmail.com>
2023-03-31 22:53:42 +0100

    Added more phone number prefix

--- src/NettyFinder.Net/Network.cs
+++ src/NettyFinder.Net/Network.cs
@@ -5,7 +5,7 @@ namespace NettyFinder.Net
 {
     public class Network
     {
-        private string phoneNumber;
+        private string _phoneNumber;
 
         public Network(string phoneNumber)
         {
@@ -13,7 +13,7 @@ namespace NettyFinder.Net
             {
                 throw new ArgumentException($"'{nameof(phoneNumber)}' cannot be null or empty.", nameof(phoneNumber));
             }
-            this.phoneNumber = phoneNumber;
+            _phoneNumber = phoneNumber;
         }
 
         public string GetNetworkName()
@@ -24,10 +24,9 @@ namespace NettyFinder.Net
             var secondaryPhonePrefix = GetPhonePrefix(5);
 
             if (Constants.starcomms.Contains(primaryPhonePrefix) || Constants.starcomms.Contains(secondaryPhonePrefix)) return Constants.Starcomms;
-            if (Constants.multilinks.Contains(primaryPhonePrefix) || Constants.multilinks.Contains(secondaryPhonePrefix)) return Constants.Multilinks;
-            if (Constants.visafone.Contains(primaryPhonePrefix) || Constants.visafone.Contains(secondaryPhonePrefix)) return Constants.Visafone;
+            if (Constants.multilinks.Contains(primaryPhonePrefix) || Constants.multilinks.Contains(secondaryPhonePrefix)) return Constants.Multilinks; 
 
-            if (Constants.mtn.Contains(primaryPhonePrefix)) return Constants.Mtn;
+            if (Constants.mtn.Contains(primaryPhonePrefix) || Constants.mtn.Contains(secondaryPhonePrefix)) return Constants.Mtn;
             if (Constants.glo.Contains(primaryPhonePrefix)) return Constants.Glo;
             if (Constants.airtel.Contains(primaryPhonePrefix)) return Constants.Airtel;
             if (Constants.etisalat.Contains(primaryPhonePrefix)) return Constants.EtisalatMobile;
@@ -41,37 +40,37 @@ namespace NettyFinder.Net
 
         public string GetPhonePrefix(int length = 4)
         {
-            return phoneNumber.Substring(0, length);
+            return _phoneNumber.Substring(0, length);
         }
 
         public bool ValidatePhoneNumber()
         {
-            var prefix = phoneNumber.Substring(0,4);
-            var phoneNumberEdited = phoneNumber.StartsWith("+") ? phoneNumber.Substring(1) : phoneNumber;
+            var prefix = _phoneNumber.Substring(0,4);
+            var phoneNumberEdited = _phoneNumber.StartsWith("+") ? _phoneNumber.Substring(1) : _phoneNumber;
             if (!phoneNumberEdited.All(c => char.IsDigit(c)))
             {
                 throw new Exception("Phone number contains unwanted characters");
             }
 
-            if(phoneNumber.Length < 11)
+            if(_phoneNumber.Length < 11)
             {
                 throw new Exception("Phone number cannot be less than 11 digit.");
             }
 
             //Check if number without +234 is greater than 11
-            if (phoneNumber.Length > 11 && prefix != "+234")
+            if (_phoneNumber.Length > 11 && prefix != "+234")
             {
                 throw new Exception("Number without +234 must not be greater than 11 digits");
             }
 
             // Check if +234 number is less than 14 characters
-            if (phoneNumber.Length < 14 && prefix == "+234")
+            if (_phoneNumber.Length < 14 && prefix == "+234")
             {
                 throw new Exception("Number with +234 must be 14 characters long");
             }
 
             //Check if number with +234 is greater than 14 characters
-            if (this.phoneNumber.Length > 14 && prefix == "+234")
+            if (_phoneNumber.Length > 14 && prefix == "+234")
             {
                 throw new Exception("Number with +234 must not be greater than 14 characters" );
             }
@@ -79,7 +78,7 @@ namespace NettyFinder.Net
             //Convert it to normal 08030XXXXXXX
             if (prefix == "+234")
             {
-                phoneNumber = $"0{phoneNumber.Substring(4)}";
+                _phoneNumber = $"0{_phoneNumber.Substring(4)}";
             }
 
             return true;
